a29d89aaaddabf459f78fc74f567a722bd168aff
Liége Maria <83604921+LiegeMariah@users.noreply.github.com>
2021-05-11 21:02:33 -0300

    ml

--- app/src/main/java/com/example/plantpart/MainActivity.kt
+++ app/src/main/java/com/example/plantpart/MainActivity.kt
@@ -1,18 +1,57 @@
 package com.example.plantpart
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import com.example.plantpart.ui.main.MainFragment
 
-class MainActivity : AppCompatActivity() {
+class ActivityLogin : AppCompatActivity() {
 
+    private val SENHA = "12345";
+    lateinit var btn_login: Button
+
+
+    //esse e o metodfo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+        //setando o nome da tela
+        setContentView(R.layout.main_fragment)
+
+//teste
+        buscarComponentesPeloId();
+
+        //terminar esse projeto
+
+        //botão Logingit add
+        btn_login.setOnClickListener(){
+            println("clicou no botão")}
+            val et_senha =  findViewById<EditText>(R.id.et_senha)
+            val senha = et_senha.getText().toString();
+
+            //Reconhecer senha
+            if(SENHA.equals(senha)) {
+                val intent = Intent(this, MainFragment::class.java)
+                startActivity(intent)
+            }else{
+          //      Snackbar.make(it, "senha inválida", Snackbar.LENGTH_SHORT).show()
+            }
         }
+
+
+
+
+
+
     }
-}
+
+    fun buscarComponentesPeloId(){
+      //  Button = findViewById<Button>(R.id.btn_login)
+        //testeEmail = findViewById<TextView>(R.id.et_email)
+    }
+
+
+
+
+
